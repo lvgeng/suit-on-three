ac4365fa3287ced3f2420ff55216070712a8b9c8
lvgeng <lvgeng@localhost.localdomain>
2015-05-24 17:16:14 +0800

    a huge change of the code. adding LoL charactor and its texture without modifying the material

--- javascript/suit_on_web_main.js
+++ javascript/suit_on_web_main.js
@@ -5,47 +5,96 @@ var container, stats;
 var camera, controls, scene, renderer;
 
 init();
-render();
+animate();
 
 //===================================================================================================
 function animate() {
 	requestAnimationFrame(animate);
 	controls.update();
+	render();
 }
 
 function init() {
 
 	scene = new THREE.Scene();
-	// scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
-	// world
-	var geometry = new THREE.BoxGeometry( 20,20,20 );
-	var material =  new THREE.MeshLambertMaterial( { color:0x999999, shading: THREE.FlatShading } );
+	scene.fog = new THREE.Fog( 0xcce0ff, 50, 500 );
 
-	for ( var i = 0; i < 500; i ++ ) {
 
-		var mesh = new THREE.Mesh( geometry, material );
-		mesh.position.x = ( Math.random() - 0.5 ) * 1000;
-		mesh.position.y = ( Math.random() - 0.5 ) * 1000;
-		mesh.position.z = ( Math.random() - 0.5 ) * 1000;
-		mesh.updateMatrix();
-		mesh.matrixAutoUpdate = false;
-		scene.add( mesh );
-	}
-	// lights
+	var light;
+	scene.add( new THREE.AmbientLight( 0x666666 ) );
 
-	light = new THREE.DirectionalLight( 0xffffff );
-	light.position.set( 1, 1.5, 2 ).normalize();
+	light = new THREE.DirectionalLight( 0xdfebff, 1.75 );
+	light.position.set( 50, 200, 100 );
+	light.position.multiplyScalar( 1.3 );
+
+	light.castShadow = true;
+	light.shadowCameraVisible = true;
+
+	light.shadowMapWidth = 1024;
+	light.shadowMapHeight = 1024;
+
+	var d = 300;
+
+	light.shadowCameraLeft = -d;
+	light.shadowCameraRight = d;
+	light.shadowCameraTop = d;
+	light.shadowCameraBottom = -d;
+
+	light.shadowCameraFar = 1000;
+	light.shadowDarkness = 0.5;
+
 	scene.add( light );
 
-	light = new THREE.DirectionalLight( 0x0044aa );
-	light.position.set( -1, -1.5, -1.9 );
-	scene.add( light );
+	var groundTexture = THREE.ImageUtils.loadTexture( "/assets/textures/grasslight-big.jpg" );
+	groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+	groundTexture.repeat.set( 25, 25 );
+	groundTexture.anisotropy = 16;
 
-	light = new THREE.AmbientLight( 0x222222 );
-	scene.add( light );
+	//ground
+	var groundMaterial = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x111111, map: groundTexture } );
+	var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 200, 200 ), groundMaterial );
+	mesh.position.y = 0;
+	mesh.rotation.x = - Math.PI / 2;
+	mesh.receiveShadow = true;
+	scene.add( mesh );
+
+
+	var manager = new THREE.LoadingManager();
+	manager.onProgress = function ( item, loaded, total ) {
+
+		console.log( item, loaded, total );
+
+	};
+
+	var onProgress = function ( xhr ) {
+		if ( xhr.lengthComputable ) {
+			var percentComplete = xhr.loaded / xhr.total * 100;
+			console.log( Math.round(percentComplete, 2) + '% downloaded' );
+		}
+	};
+
+	var onError = function ( xhr ) {
+	};
+
+
+	// // model
+	var loader = new THREE.OBJLoader( manager );
+
+	THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+
+				var loader = new THREE.OBJMTLLoader();
+				loader.load( '/assets/models/models_for_test/Lol_Katarina_Default/Lol_Katarina_Default.obj',
+					'/assets/models/models_for_test/Lol_Katarina_Default/Lol_Katarina_Default.mtl',
+					function ( object ) {
+						object.scale.x = 2;
+						object.scale.y = 2;
+						object.scale.z = 2;
+						scene.add( object );
+					},
+					onProgress, onError );
+
 
 	// renderer
-	container_to_render = document.getElementById("container_for_three");
 	canvas_to_render = document.getElementById('canvas_for_three');
 
 	renderer = new THREE.WebGLRenderer( { canvas: canvas_to_render, antialias: true } );
@@ -55,16 +104,17 @@ function init() {
 	renderer.setSize( canvas_to_render.scrollWidth, canvas_to_render.scrollHeight, false);
 
 	camera = new THREE.PerspectiveCamera( 30, renderer.domElement.width / renderer.domElement.height , 1, 10000 );
-	camera.position.z = 500;
+	camera.position.z = 30;
+	camera.position.y = 10;
 
 	controls = new THREE.OrbitControls( camera, renderer.domElement);
+	controls.target = new THREE.Vector3(0,7,0);
 	controls.damping = 0.2;
 	controls.addEventListener( 'change', render );
 
 	window.addEventListener( 'resize', onWindowResize, false );
 
 	animate();
-
 }
 
 
@@ -78,6 +128,5 @@ function onWindowResize() {
 }
 
 function render() {
-
 	renderer.render( scene, camera );
 }
