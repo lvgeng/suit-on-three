086ef9bb51727ae162182370962c190e81a2142a
lvgeng <lvgeng@localhost.localdomain>
2015-05-28 09:49:23 +0800

    adding selectboxes in HTML

--- javascript/suit_on_web_main.js
+++ javascript/suit_on_web_main.js
@@ -1,9 +1,7 @@
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 var container, stats;
-
-var modeles_selectbox;
-
+// var modeles_selectbox;
 var camera, controls, scene, renderer;
 
 init();
@@ -34,63 +32,61 @@ function init() {
 
 
 	// // model
-	// var loader = new THREE.OBJLoader( manager );
-	modeles_selectbox = document.getElementById("models_selector");
-	modeles_selectbox.addEventListener("change", function(){
-		switch(modeles_selectbox.selectedOptions[0].getAttribute("type")){
-			case "OBJMTL":
-				var loader = new THREE.OBJMTLLoader(manager);
-				loader.load( '/assets/models/'+modeles_selectbox.selectedOptions[0].getAttribute("path")+'.obj',
-					'/assets/models/'+modeles_selectbox.selectedOptions[0].getAttribute("path")+'.mtl',
-					function ( object ) {
-						object.scale.x = modeles_selectbox.selectedOptions[0].getAttribute("scale");
-						object.scale.y = modeles_selectbox.selectedOptions[0].getAttribute("scale");
-						object.scale.z = modeles_selectbox.selectedOptions[0].getAttribute("scale");
+	// // var loader = new THREE.OBJLoader( manager );
+	// modeles_selectbox = document.getElementById("models_selector");
+	// modeles_selectbox.addEventListener("change", function(){
+	// 	switch(modeles_selectbox.selectedOptions[0].getAttribute("type")){
+	// 		case "OBJMTL":
+	// 			var loader = new THREE.OBJMTLLoader(manager);
+	// 			loader.load( '/assets/models/'+modeles_selectbox.selectedOptions[0].getAttribute("path")+'.obj',
+	// 				'/assets/models/'+modeles_selectbox.selectedOptions[0].getAttribute("path")+'.mtl',
+	// 				function ( object ) {
+	// 					object.scale.x = modeles_selectbox.selectedOptions[0].getAttribute("scale");
+	// 					object.scale.y = modeles_selectbox.selectedOptions[0].getAttribute("scale");
+	// 					object.scale.z = modeles_selectbox.selectedOptions[0].getAttribute("scale");
 
 
-						object.name = "model_to_show";
+	// 					object.name = "model_to_show";
 
-						scene.remove(scene.getObjectByName("model_to_show"))				
-						scene.add( object );
-					},
-					onProgress, onError );
-				break;
-			case "OBJ":
-				break;
-			default:
-			console.log("undefined way to deal with the modele");
-		}		
-	})
+	// 					scene.remove(scene.getObjectByName("model_to_show"));				
+	// 					scene.add( object );
+	// 				},
+	// 				onProgress, onError );
+	// 			break;
+	// 		case "OBJ":
+	// 			break;
+	// 		default:
+	// 		console.log("undefined way to deal with the modele");
+	// 	}		
+	// })
 
-	// THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader(manager) );
-	// I cannot understand why it is here, but I think it is better to keep it here for a while until I figured out whether it is necessary
-
-	var loader = new THREE.OBJMTLLoader(manager);
-	switch(modeles_selectbox.selectedOptions[0].getAttribute("type")){
-		case "OBJMTL":
-			var loader = new THREE.OBJMTLLoader(manager);
-			loader.load( '/assets/models/'+modeles_selectbox.selectedOptions[0].getAttribute("path")+'.obj',
-				'/assets/models/'+modeles_selectbox.selectedOptions[0].getAttribute("path")+'.mtl',
-				function ( object ) {
-					object.scale.x = modeles_selectbox.selectedOptions[0].getAttribute("scale");
-					object.scale.y = modeles_selectbox.selectedOptions[0].getAttribute("scale");
-					object.scale.z = modeles_selectbox.selectedOptions[0].getAttribute("scale");
-					object.name = "model_to_show";
-
-					scene.remove(scene.getObjectByName("model_to_show"))				
-					scene.add( object );
-				},
-				onProgress, onError );
-			break;
-		case "OBJ":
-			break;
-		default:
-		console.log("undefined way to deal with the modele");
-	}
-
+	// // THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader(manager) );
+	// // I cannot understand why it is here, but I think it is better to keep it here for a while until I figured out whether it is necessary
 
 	// var loader = new THREE.OBJMTLLoader(manager);
-	
+	// switch(modeles_selectbox.selectedOptions[0].getAttribute("type")){
+	// 	case "OBJMTL":
+	// 		var loader = new THREE.OBJMTLLoader(manager);
+	// 		loader.load( '/assets/models/'+modeles_selectbox.selectedOptions[0].getAttribute("path")+'.obj',
+	// 			'/assets/models/'+modeles_selectbox.selectedOptions[0].getAttribute("path")+'.mtl',
+	// 			function ( object ) {
+	// 				object.scale.x = modeles_selectbox.selectedOptions[0].getAttribute("scale");
+	// 				object.scale.y = modeles_selectbox.selectedOptions[0].getAttribute("scale");
+	// 				object.scale.z = modeles_selectbox.selectedOptions[0].getAttribute("scale");
+	// 				object.name = "model_to_show";
+
+	// 				scene.remove(scene.getObjectByName("model_to_show"));				
+	// 				scene.add( object );
+	// 			},
+	// 			onProgress, onError );
+	// 		break;
+	// 	case "OBJ":
+	// 		break;
+	// 	default:
+	// 	console.log("undefined way to deal with the modele");
+	// }
+
+	// var loader = new THREE.OBJMTLLoader(manager);
 	// 		var loader = new THREE.OBJLoader(manager);
 	// 		loader.load( '/assets/models/models_for_test/Lol_Katarina_Default/Lol_Katarina_Default.obj',
 	// 			function ( object ) {
@@ -99,10 +95,26 @@ function init() {
 	// 				object.scale.z = 2;
 	// 				object.name = "model_to_show";
 
-	// 				scene.remove(scene.getObjectByName("model_to_show"))				
+	// 				scene.remove(scene.getObjectByName("model_to_show"));				
 	// 				scene.add( object );
 	// 			},
 	// 			onProgress, onError );
+
+	var loader = new THREE.OBJMTLLoader(manager);
+	loader.load( '/assets/models/models_for_test/Lol_Katarina_Default/Lol_Katarina_Default.obj',
+		'/assets/models/models_for_test/Lol_Katarina_Default/Lol_Katarina_Default.mtl',
+		function ( object ) {
+			object.scale.x = 2;
+			object.scale.y = 2;
+			object.scale.z = 2;
+			object.name = "model_to_show";
+
+			scene.remove(scene.getObjectByName("model_to_show"));				
+			scene.add( object );
+		},
+	onProgress, onError );
+
+
 
 	// renderer
 	canvas_to_render = document.getElementById('canvas_for_three');
@@ -110,16 +122,12 @@ function init() {
 	renderer = new THREE.WebGLRenderer( { canvas: canvas_to_render, antialias: true } );
 
 	// renderer.setClearColor( 0x8888ff );
-	renderer.setClearColor( 0x222222 );
+	renderer.setClearColor( 0xcccccc );
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( canvas_to_render.scrollWidth, canvas_to_render.scrollHeight, false);
 	renderer.shadowMapEnabled = true;
 	renderer.shadowMapSoft = true;
 	renderer.shadowMapType = THREE.PCFSoftShadowMap;
-
-
-
-
 
 
 
@@ -150,3 +158,4 @@ function onWindowResize() {
 function render() {
 	renderer.render( scene, camera );
 }
+
