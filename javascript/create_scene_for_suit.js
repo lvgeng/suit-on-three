94b75d20d4efcb663efdcb29e47155d13f0f9d48
lvgeng <lvgeng@localhost.localdomain>
2015-05-30 16:47:40 +0800

    almost done and there is part of a suit

--- javascript/create_scene_for_suit.js
+++ javascript/create_scene_for_suit.js
@@ -15,11 +15,21 @@ function create_scene_basic() {
 	var light = new THREE.AmbientLight( 0xffffff);
 	light.name = "ambientlight";
 	scene.add( light );
+
+	var directionalLight = new THREE.DirectionalLight( 0xffffff );
+	directionalLight.position.set( 0, 0, 1 ).normalize();
+	scene.add( directionalLight );
+
+	var directionalLight = new THREE.DirectionalLight( 0xffffff );
+	directionalLight.position.set( 0, 0, -1 ).normalize();
+	scene.add( directionalLight );
+
+
 	
 	// Grid
 	var line_material = new THREE.LineBasicMaterial( { color: 0x999999 } ),
 	geometry = new THREE.Geometry(),
-	floor = 0, step = 10;
+	floor = -20, step = 10;
 
 	for ( var i = 0; i <= 20; i ++ ) {
 		geometry.vertices.push( new THREE.Vector3( - 100, floor, i * step - 100 ) );
@@ -50,14 +60,14 @@ function create_renderer(canvas_to_render){
 
 function create_camera(controls_for_camera){
 	var camera = new THREE.PerspectiveCamera( 30, renderer.domElement.width / renderer.domElement.height , 1, 1000 );
-	camera.position.z = 15;
-	camera.position.y = 5;
+	camera.position.z = 100;
+	camera.position.y = 0;
 	return camera;
 }
 
 function creat_controls_for_camera(camera, canvas_to_render){
 	var controls = new THREE.OrbitControls( camera, renderer.domElement);
-	controls.target = new THREE.Vector3(0,3.5,0);
+	controls.target = new THREE.Vector3(0,0,0);
 	controls.noZoom = true;
 	controls.noPan = true;
 	controls.minPolarAngle = Math.PI/13*6; // radians
@@ -93,11 +103,11 @@ function update_suit_model(model_name,scene, manager_for_loading, path_OBJ, Path
 // var selectbox_button_style, selectbox_back_bottom, selectbox_collar_style, selectbox_pocket_style;
 // var select_fabric;
 
-function update_suit_main(scene, manager_for_loading){
+function update_suit_main(manager_for_loading){
 	var suit_main_name = selectbox_button_style.value+'_'+selectbox_back_bottom.value;
 	var selected_fabric = select_fabric.getAttribute("selectedfabric");
-	var path_OBJ_suit_main = "/assets/suit_main/" + suit_main_name + '/' + suit_main_name +'.obj';
-	var path_MTL_suit_main = "/assets/suit_main/" + suit_main_name + '/' + suit_main_name + '_' + selected_fabric + '.mtl';
+	var path_OBJ_suit_main = "/assets/models/suit_main/" + suit_main_name + '/' + suit_main_name +'.obj';
+	var path_MTL_suit_main = "/assets/models/suit_main/" + suit_main_name + '/' + suit_main_name + '_' + selected_fabric + '.mtl';
 
 	console.log("OBJ: " + path_OBJ_suit_main);
 	console.log("MTL: " + path_MTL_suit_main);
@@ -107,25 +117,25 @@ function update_suit_main(scene, manager_for_loading){
 }
 
 //==============================================unfinished!!!!!!!!!!=================================================================
-function update_suit_collar(scene, manager_for_loading){
+function update_suit_collar(manager_for_loading){
 	var suit_button_style_name = selectbox_button_style.value;
 	var suit_collar_name = suit_button_style_name + '_' + selectbox_collar_style.value;
 	var selected_fabric = select_fabric.getAttribute("selectedfabric");
-	var path_OBJ_suit_collar = "/assets/suit_collar/" + suit_collar_name + '/' + suit_collar_name +'.obj';
-	var path_MTL_suit_collar = "/assets/suit_collar/" + suit_collar_name + '/' + suit_collar_name + '_' + selected_fabric + '.mtl';
+	var path_OBJ_suit_collar = "/assets/models/suit_collar/" + suit_collar_name + '/' + suit_collar_name +'.obj';
+	var path_MTL_suit_collar = "/assets/models/suit_collar/" + suit_collar_name + '/' + suit_collar_name + '_' + selected_fabric + '.mtl';
 
 	console.log("OBJ: " + path_OBJ_suit_collar);
 	console.log("MTL: " + path_MTL_suit_collar);
 
-	// update_suit_model("suit_collar", scene, manager_for_loading, path_OBJ_suit_collar, path_MTL_suit_collar);
+	update_suit_model("suit_collar", scene, manager_for_loading, path_OBJ_suit_collar, path_MTL_suit_collar);
 }
 //==============================================================================================================
 
-function update_suit_pocket(scene, manager_for_loading){
+function update_suit_pocket(manager_for_loading){
 	var suit_pocket_name = selectbox_pocket_style.value;
 	var selected_fabric = select_fabric.getAttribute("selectedfabric");
-	var path_OBJ_suit_pocket = "/assets/suit_pocket/" + suit_pocket_name + '/' + suit_pocket_name +'.obj';
-	var path_MTL_suit_pocket = "/assets/suit_pocket/" + suit_pocket_name + '/' + suit_pocket_name + '_' + selected_fabric + '.mtl';
+	var path_OBJ_suit_pocket = "/assets/models/suit_pocket/" + suit_pocket_name + '/' + suit_pocket_name +'.obj';
+	var path_MTL_suit_pocket = "/assets/models/suit_pocket/" + suit_pocket_name + '/' + suit_pocket_name + '_' + selected_fabric + '.mtl';
 
 	console.log("OBJ: " + path_OBJ_suit_pocket);
 	console.log("MTL: " + path_MTL_suit_pocket);
@@ -133,7 +143,7 @@ function update_suit_pocket(scene, manager_for_loading){
 	// update_suit_model("suit_pocket", scene, manager_for_loading, path_OBJ_suit_pocket, path_MTL_suit_pocket);
 }
 
-function update_fabric(scene, manager_for_loading){
+function update_fabric(manager_for_loading){
 	update_suit_main(scene, manager_for_loading);
 	// update_suit_collar();
 	update_suit_pocket(scene, manager_for_loading);
--- javascript/suit_on_web_main.js
+++ javascript/suit_on_web_main.js
@@ -44,11 +44,31 @@ function init() {
 
 	update_suit_model("suit_main", 
 		scene, manager_for_loading, 
-		'/assets/models/models_for_test/Lol_Katarina_Default/Lol_Katarina_Default.obj',
-		'/assets/models/models_for_test/Lol_Katarina_Default/Lol_Katarina_Default.mtl');
-	
+		'/assets/models/suit_main/single-breasted_2_side/single-breasted_2_side.obj',
+		'/assets/models/suit_main/single-breasted_2_side/single-breasted_2_side_fabric_black.mtl');
+
+	update_suit_model("suit_collar", 
+		scene, manager_for_loading, 
+		'/assets/models/suit_collar/single-breasted_2_peaked_lapel/single-breasted_2_peaked_lapel.obj',
+		'/assets/models/suit_collar/single-breasted_2_peaked_lapel/single-breasted_2_peaked_lapel_fabric_blue.mtl');
+
+	// update_suit_model("models_for_test", 
+	// 	scene, manager_for_loading, 
+	// 	'/assets/models/models_for_test/Lol_Katarina_Default/Lol_Katarina_Default.obj',
+	// 	'/assets/models/models_for_test/Lol_Katarina_Default/Lol_Katarina_Default.mtl');
+
+	// console.log(scene);
+	// console.log(scene.getObjectByName("models_for_test"));
+
+	// scene.getObjectByName("models_for_test").scale.x = 5;
+	// scene.getObjectByName("models_for_test").scale.y = 5;
+	// scene.getObjectByName("models_for_test").scale.z = 5;
+
 	animate();
 }
+
+	
+
 
 //===========================================================================================
 
